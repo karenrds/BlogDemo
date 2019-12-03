bea652ffe2ed6b6194696973ad3248d39b3288f3
Karen Rodrigues <karen.rodrigues30@gmail.com>
2019-12-03 23:01:56 +0000

    Second Commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,7 @@
                     <ul class="navbar-nav mr-auto">
 
                     </ul>
+                    
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
